cbb897010df3ff3bc2983ce5ec01e0ceacfdf655
Andrew Naylor <argon@mkbot.net>
2016-07-07 18:35:01 +0100

    Simplify message handling logic

--- push_notify.js
+++ push_notify.js
@@ -8,32 +8,36 @@ const apn = require('apn');
 
 const server = net.createServer( connection => {
   c.on('data', data => {
-    const msg_data = {
-      msg: data.readUInt32LE(0),
-      pid: data.readUInt32LE(4),
-
-      d1: fromCString(data.slice(8,     135), 'utf8', 0),
-      d2: fromCString(data.slice(136,   647), 'utf8', 0),
-      d3: fromCString(data.slice(648,  1159), 'utf8', 0),
-      d4: fromCString(data.slice(1160, 1671), 'utf8', 0),
-    };
-
-    if(msg_data.msg == 1) {
-      console.error("Create node, msg 1 is deprecated");
-    }
-    else if(msg_data.msg == 2) {
-      console.log("Registering client", msg_data.d1, msg_data.d2);
-      register_client(msg_data);
-    }
-    else if(msg_data.msg == 3) {
-      console.log("Publishing notification", msg_data.d1);
-      publish(msg_data);
-    }
-    else {
-      console.error("Error: unknown message type: ", msg_data.msg);
+    const message = parseMessage(data)
+    switch (message.type) {
+      case 1:
+        console.error("Create node, type 1 is deprecated");
+        break;
+      case 2:
+        console.log("Registering client", message.d1, message.d2);
+        register_client(message);
+        break;
+      case 3:
+        console.log("Publishing notification", message.d1);
+        publish(message);
+        break;
+      default:
+        console.error("Error: unknown message type: ", message.type);
     }
   });
 });
+
+function parseMessage(data) {
+  return {
+    type: data.readUInt32LE(0),
+    pid: data.readUInt32LE(4),
+
+    d1: fromCString(data.slice(8,     135), 'utf8', 0),
+    d2: fromCString(data.slice(136,   647), 'utf8', 0),
+    d3: fromCString(data.slice(648,  1159), 'utf8', 0),
+    d4: fromCString(data.slice(1160, 1671), 'utf8', 0),
+  };
+}
 
 var redis_client = redis.createClient();
 var connection = new apn.Connection({
@@ -47,11 +51,11 @@ feedback.on('feedback', function(data) {
   }
 });
 
-function publish(msg_data) {
-  redis_client.smembers("pn:email:" + msg_data.d1, function (err, replies) {
+function publish(message) {
+  redis_client.smembers("pn:email:" + message.d1, function (err, replies) {
     if(replies === null) return;
     replies.forEach(function(value, i) {
-      redis_client.get("pn:accountid:" + msg_data.d1 + ":" + value, function(err, accountId) {
+      redis_client.get("pn:accountid:" + message.d1 + ":" + value, function(err, accountId) {
         if(accountId === null) return;
         // Send push notification
         var device = new apn.Device(value);
@@ -64,13 +68,13 @@ function publish(msg_data) {
   });
 }
 
-function register_client(msg_data) {
-  redis_client.sadd("pn:email:" + msg_data.d1, msg_data.d3);
-  redis_client.set("pn:accountid:" + msg_data.d1 + ":" + msg_data.d3, msg_data.d2);
-  redis_client.sadd("pn:device:" + msg_data.d3, msg_data.d1);
+function register_client(message) {
+  redis_client.sadd("pn:email:" + message.d1, message.d3);
+  redis_client.set("pn:accountid:" + message.d1 + ":" + message.d3, message.d2);
+  redis_client.sadd("pn:device:" + message.d3, message.d1);
 
   var timestamp = Math.round((new Date()).getTime() / 1000);
-  redis_client.set("pn:device:" + msg_data.d3 + ":time", timestamp);
+  redis_client.set("pn:device:" + message.d3 + ":time", timestamp);
 }
 
 function unregister_device(deviceId, timestamp) {
