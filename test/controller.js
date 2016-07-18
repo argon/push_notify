61a8d9c29b3370df36735abd96d15c609e56fac6
Andrew Naylor <argon@mkbot.net>
2016-07-18 22:00:42 +0100

    Refactor registration to use template strings

--- test/controller.js
+++ test/controller.js
@@ -18,21 +18,19 @@ describe("Controller", function() {
   });
 
   describe("register", function() {
-    it("registers the device token in redis", function() {
+    beforeEach(function () {
       controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
+    });
 
+    it("registers the device token in redis", function() {
       expect(fakes.redis.sadd).to.be.calledWith("test_pn_prefix:test@example.com:device", "1234567890abcdef");
     });
 
     it("registers the account id in redis", function() {
-      controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
-
       expect(fakes.redis.set).to.be.calledWith("test_pn_prefix:test@example.com:1234567890abcdef:accountid", "1234abcd");
     });
 
     it("registers the username to the device token in redis", function() {
-      controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
-
       expect(fakes.redis.sadd).to.be.calledWith("test_pn_prefix:1234567890abcdef:user", "test@example.com");
     });
   });
