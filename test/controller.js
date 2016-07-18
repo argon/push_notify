1c9e1c60011967f2bbb909d2734de2ec5a8501d5
Andrew Naylor <argon@mkbot.net>
2016-07-18 21:43:57 +0100

    Implement device registration

---
+++ test/controller.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const Controller = require("../lib/controller");
+
+describe("Controller", function() {
+  let fakes;
+  let controller;
+
+  beforeEach(function () {
+    fakes = {
+      apn:   new mock.APNConnection(),
+      redis: new mock.RedisClient(),
+    }
+
+    fakes.redis.keyPrefix = "test_pn_prefix:";
+
+    controller = new Controller(fakes);
+  });
+
+  describe("register", function() {
+    it("registers the device token in redis", function() {
+      controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
+
+      expect(fakes.redis.sadd).to.be.calledWith("test_pn_prefix:test@example.com:device", "1234567890abcdef");
+    });
+
+    it("registers the account id in redis", function() {
+      controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
+
+      expect(fakes.redis.set).to.be.calledWith("test_pn_prefix:test@example.com:1234567890abcdef:accountid", "1234abcd");
+    });
+
+    it("registers the username to the device token in redis", function() {
+      controller.register("test@example.com", "1234abcd", "1234567890abcdef", "io.github.argon.push");
+
+      expect(fakes.redis.sadd).to.be.calledWith("test_pn_prefix:1234567890abcdef:user", "test@example.com");
+    });
+  });
+});
--- test/support.js
+++ test/support.js
@@ -11,13 +11,58 @@ global.expect = chai.expect;
 
 const sinon = require("sinon");
 const Promise = require("bluebird");
+function APNConnection(sender) {
+  this.sender = sinon.stub();
+  this.sender.returns({status: "200"});
+}
+
+APNConnection.prototype.pushNotification = function pushNotification(notification, recipients) {
+  const builtNotification = {
+    headers: notification.headers(),
+    body:    notification.compile(),
+  };
+
+  if (!Array.isArray(recipients)) {
+    recipients = [recipients];
+  }
+
+  return Promise.all( recipients.map(this.write.bind(this, builtNotification)) )
+    .then( responses => {
+    let sent = [];
+    let failed = [];
+
+    responses.forEach( response => {
+      if (response.status) {
+        failed.push(response);
+      } else {
+        sent.push(response);
+      }
+    });
+    return {sent, failed};
+  });
+}
+
 function Controller() {
   this.register  = sinon.stub();
   this.notify    = sinon.stub();
   this.subscribe = sinon.stub();
 }
 
+function RedisClient() {
+  this.sadd      = sinon.stub();
+  this.set       = sinon.stub();
+  this.smembers  = sinon.stub();
+}
+
+RedisClient.prototype.sadd     = function () {};
+RedisClient.prototype.set      = function () {};
+RedisClient.prototype.smembers = function () {};
+
+Promise.promisifyAll(RedisClient.prototype);
+
 global.mock = {
+  APNConnection,
   Controller,
+  RedisClient,
 }
 
