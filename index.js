6d8cf6b4c0a62ff83fb60e7a08072e038108bbfd
Andrew Naylor <argon@mkbot.net>
2016-07-27 00:27:01 +0100

    Restructure the loader
    This is to ensure we don't open redis earlier than we need to, ensuring
    that errors creating the socket will cause the process to exit.

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const Promise = require("bluebird");
 
 Promise.promisifyAll(redis.RedisClient.prototype);
 
-const redisURL    = process.env["REDIS_URL"];
-const redisPrefix = process.env["REDIS_PREFIX"];
-const redisClient = redis.createClient({url: redisURL});
-const apnConnection = new apn.Connection({ production: true });
-
 const Controller = require("./lib/controller")({Notification: apn.Notification});
 const Server = require("./lib/server");
 const Socket = require("./lib/socket");
 
-const controller = new Controller({ redis: redisClient, apn: apnConnection, prefix: redisPrefix });
-const server     = new Server({ controller });
-
 Socket("/var/dovecot/push_notify")
   .then( socket => {
+    const redisURL    = process.env["REDIS_URL"];
+    const redisPrefix = process.env["REDIS_PREFIX"];
+    const redisClient = redis.createClient({url: redisURL});
+    const apnConnection = new apn.Connection({ production: true });
+
+    const controller = new Controller({ redis: redisClient, apn: apnConnection, prefix: redisPrefix });
+    const server     = new Server({ controller });
+
     socket.on("connection", connection => {
       connection.on("data", server.receive.bind(server));
     });
