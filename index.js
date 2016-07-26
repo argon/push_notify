dc592246e8469721de13c5f0880c9cbb8420b14a
Andrew Naylor <argon@mkbot.net>
2016-07-26 12:23:30 +0100

    Tidy up index

--- index.js
+++ index.js
@@ -3,16 +3,18 @@
 const apn = require("apn");
 const redis = require("redis");
 
-const redisClient = redis.createClient({url: process.env["REDIS_URL"]});
+const redisURL    = process.env["REDIS_URL"];
+const redisPrefix = process.env["REDIS_PREFIX"];
+const redisClient = redis.createClient({url: redisURL});
 const apnConnection = new apn.Connection({});
 
 const Controller = require("./lib/controller")({Notification: apn.Notification});
 const Server = require("./lib/server");
 const Socket = require("./lib/socket");
 
-const controller = new Controller({ redis: redisClient, apn: apnConnection, prefix: process.env["REDIS_PREFIX"] });
-const server = new Server({ controller });
-const socket = new Socket("/tmp/dovecot.sock")
+const controller = new Controller({ redis: redisClient, apn: apnConnection, prefix: redisPrefix });
+const server     = new Server({ controller });
+const socket     = new Socket("/tmp/dovecot.sock")
   .then( socket => {
     socket.on("data", server.receive.bind(server));
   });
