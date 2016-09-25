cda4f6c3e037f13a96e5592cf3c674475da0fdeb
Andrew Naylor <argon@mkbot.net>
2016-09-25 01:46:25 +0100

    Only notify for subscribed mailboxes

--- test/controller.js
+++ test/controller.js
@@ -14,6 +14,7 @@ describe("Controller", function() {
       prefix: "test_pn_prefix:",
     }
 
+    fakes.md5.withArgs("INBOX").returns("md5-value");
     fakes.apn.client.write = sinon.stub().returns({});
 
     const Controller = require("../lib/controller")({
@@ -39,32 +40,97 @@ describe("Controller", function() {
   });
 
   describe("notify", function() {
-    it("pushes to each registered device token", function () {
-      const notify = controller.notify("test@example.com", "INBOX");
-      fakes.redis.smembers.yield(null, ["123456abcdef:account-id-1234", "4567890fedcba:account-id-4567"]);
+    let notify;
+    describe("push behaviour", function () {
 
       const expectedNotification = function (accountId) {
-        const expectedBody = JSON.stringify({"aps": { "account-id": accountId}});
+        const expectedBody = JSON.stringify({"aps": { "account-id": accountId, m: [ "md5-value" ]}});
         return sinon.match(function (value) {
           return value.body == expectedBody;
         }, expectedBody);
       };
 
-      return notify.then( () => {
-        expect(fakes.apn.client.write).to.have.been.calledWith(expectedNotification("account-id-1234"), "123456abcdef");
-        expect(fakes.apn.client.write).to.have.been.calledWith(expectedNotification("account-id-4567"), "4567890fedcba");
+      beforeEach(function () {
+        notify = controller.notify("test@example.com", "INBOX");
+        
+        fakes.redis.sismember.withArgs("test_pn_prefix:test@example.com:123456abcdef:account-id-1234:subscriptions", "md5-value").yields(null, 1);
+        fakes.redis.sismember.withArgs("test_pn_prefix:test@example.com:123456abcdef:account-id-4567:subscriptions", "md5-value").yields(null, 0);
+        fakes.redis.sismember.withArgs("test_pn_prefix:test@example.com:4567890fedcba:account-id-4567:subscriptions", "md5-value").yields(null, 1);
+
+        fakes.redis.smembers.yield(null, [
+          "123456abcdef:account-id-1234", 
+          "123456abcdef:account-id-4567",
+          "4567890fedcba:account-id-4567",
+        ]);
+      });
+
+      it("pushes to each registered and subscribed device token", function () {
+        return notify.then( () => {
+          expect(fakes.apn.client.write).to.have.been.calledWith(expectedNotification("account-id-1234"), "123456abcdef");
+          expect(fakes.apn.client.write).to.have.been.calledWith(expectedNotification("account-id-4567"), "4567890fedcba");
+        });
+      });
+
+      it("does not push to non-subscribed device token", function () {
+        return notify.then( () => {
+          expect(fakes.apn.client.write).to.have.not.been.calledWith(expectedNotification("account-id-4567"), "123456abcdef");
+        });
       });
     });
 
-    it("cleans up failed device tokens", function () {
-      fakes.apn.client.write.withArgs(sinon.match.any, "123456abcdef").returns( { status: 410, device: "123456abcdef", response: { reason: "Unregistered", timestamp: 0 } });
-      fakes.redis.srem.yields(null, 1);
+    context("token rejected", function () {
+      let apnWriteResolve;
+      beforeEach(function () {
+        fakes.apn.client.write.withArgs(sinon.match.any, "123456abcdef")
+          .returns(new Promise( resolve => { apnWriteResolve = resolve; }));
 
-      const notify = controller.notify("test@example.com", "INBOX");
-      fakes.redis.smembers.yield(null, ["123456abcdef:account-id-1234", "4567890fedcba:account-id-4567"]);
+        fakes.redis.srem.yields(null, 1);
+        fakes.redis.del.yields(null, 1);
 
-      return notify.then( () => {
-        expect(fakes.redis.srem).to.be.calledWith("test_pn_prefix:test@example.com:device", "123456abcdef:account-id-1234");
+        fakes.redis.sismember.withArgs("test_pn_prefix:test@example.com:123456abcdef:account-id-1234:subscriptions", "md5-value").yields(null, 1);
+        fakes.redis.sismember.withArgs("test_pn_prefix:test@example.com:4567890fedcba:account-id-4567:subscriptions", "md5-value").yields(null, 1);
+
+        notify = controller.notify("test@example.com", "INBOX");
+        
+        fakes.redis.smembers.yield(null, ["123456abcdef:account-id-1234", "4567890fedcba:account-id-4567"]);
+      });
+
+      context("`Unregistered` token", function () {
+        beforeEach(function () {
+          apnWriteResolve({ status: "410", device: "123456abcdef", response: { reason: "Unregistered", timestamp: 0 } });
+        });
+        
+        it("cleans up failed device token from user account", function () {
+          return notify.then( () => {
+            expect(fakes.redis.srem).to.be.calledOnce;
+            expect(fakes.redis.srem).to.be.calledWith("test_pn_prefix:test@example.com:device", "123456abcdef:account-id-1234");
+          });
+        });
+
+        it("deletes device subscriptions set", function (){
+          return notify.then( () => {
+            expect(fakes.redis.del).to.be.calledOnce;
+            expect(fakes.redis.del).to.be.calledWith("test_pn_prefix:test@example.com:123456abcdef:account-id-1234:subscriptions");
+          });
+        });
+      });
+
+      context("other reason", function () {
+        beforeEach(function () {
+          apnWriteResolve({ status: "503", device: "123456abcdef", response: { reason: "ServiceUnavailable" } });
+        });
+
+        it("does not cleanup the token from the user account", function () {
+          return notify.then( () => {
+            expect(fakes.redis.srem).to.not.be.called;
+          });
+        });
+
+        it("does not delete the device subscriptions set", function (){
+          return notify.then( () => {
+            expect(fakes.redis.del).to.not.be.called;
+          });
+        });
       });
     });
   });
